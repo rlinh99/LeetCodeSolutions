6b48c4cfaef91f465be6c3db3a9b59ed452ecb7a
Sen Lin <33269903+rlinh99@users.noreply.github.com>
2022-07-27 13:58:45 +0800

    update

---
+++ 114-FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,47 @@
+/*
+Solution - Recursive
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution 
+{
+    public void Flatten(TreeNode root)
+    {
+        FlattenTree(root);
+    }
+    
+    private TreeNode FlattenTree(TreeNode root)
+    {
+        if(root == null)
+            return null;
+        
+        if (root.left == null && root.right == null) 
+        {
+            return root;
+        }
+        
+        var left = FlattenTree(root.left);
+        var right = FlattenTree(root.right);
+        
+        if(left != null)
+        {
+            left.right = root.right;
+            root.right = root.left;
+            root.left = null;
+        }
+        return right == null? left:right;
+    }
+        
+}
