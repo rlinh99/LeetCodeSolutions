a139c2801ca4e6fa335ac6a49f89ade9a32e783d
Sen Lin <33269903+rlinh99@users.noreply.github.com>
2022-07-27 12:38:24 +0800

    Updated Wordbreak

---
+++ 139-WordBreak-BFS.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public bool WordBreak(string s, IList<string> wordDict) {
+        HashSet<string> hs = new HashSet<string>(wordDict);
+        Queue<int> q = new ();
+        bool[] visited = new bool[s.Length];
+        
+        q.Enqueue(0);
+        while(q.Count >0){
+            var curr = q.Dequeue();
+            if(visited[curr])
+                continue;
+            
+            visited[curr] = true;
+            for(int i = curr + 1; i <= s.Length; i++){
+                
+                if(hs.Contains(s.Substring(curr, i - curr))){
+                    if(i == s.Length)
+                        return true;
+                    
+                    q.Enqueue(i);
+                }
+            }
+        }
+        return false;
+    }
+}
