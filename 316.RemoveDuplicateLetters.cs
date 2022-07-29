a34d6053c939b3c64680a705fdddba6fbbbe4d0d
Sen Lin <33269903+rlinh99@users.noreply.github.com>
2022-07-29 17:34:51 +0800

    modified

--- 316.RemoveDuplicateLetters.cs
+++ 316.RemoveDuplicateLetters.cs
@@ -2,34 +2,27 @@ public class Solution
 {
     public string RemoveDuplicateLetters(string s)
     {
-        if (s.Length == 0)
-            return s;
-
         Dictionary<char, int> dict = new();
-
+        HashSet<char> visited = new();
+        Stack<char> stk = new();
+        //find the last position of the string.
         for (int k = 0; k < s.Length; k++)
             dict[s[k]] = k;
 
-        HashSet<char> visited = new();
-        Stack<char> stk = new();
-
         for (int i = 0; i < s.Length; i++)
         {
-            while (stk.Count > 0 &&
-                  stk.Peek() > s[i] && dict.ContainsKey(stk.Peek())
-                  && dict[stk.Peek()] > i && !visited.Contains(s[i]))
+            while (stk.Count > 0 && stk.Peek() > s[i]
+                 && dict[stk.Peek()] > i && !visited.Contains(s[i]))
             {
-                //when peek will present after current position, remove it safely
-                visited.Remove(stk.Peek());
-                stk.Pop();
+                visited.Remove(stk.Pop());
             }
+
             if (!visited.Contains(s[i]))
             {
                 stk.Push(s[i]);
                 visited.Add(s[i]);
             }
         }
-        
         return string.Join("", stk.Reverse());
     }
 }
